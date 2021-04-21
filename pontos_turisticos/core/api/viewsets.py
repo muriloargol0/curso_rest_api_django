f94aa9db59978421c2434ea79ed4efc0202b96ed
Murilo Argolo <murilo.argolo@live.com>
2021-04-20 23:04:53 -0300

    action

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -1,7 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
 class PontoTuristicoViewSet(ModelViewSet):
-    queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
+    queryset = PontoTuristico.objects.all()
+    
+    @action(methods=['get'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+        
+    
+    
